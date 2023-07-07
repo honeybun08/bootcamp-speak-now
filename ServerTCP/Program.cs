2345a47e4fa5ab41261c3edd6db99ff8c7f28eac
Ulyana <ulyana_yezhgurova@mail.ru>
2023-07-07 14:00:01 +0300

    The construction details of the web-server

---
+++ ServerTCP/Program.cs
@@ -0,0 +1,178 @@
+﻿// program.cs с получение ответа
+﻿namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Это наш клиент");
+            OurClient ourClient = new OurClient();
+        }
+    }
+}
+
+// OurClient с получение ответа
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class OurClient
+    {
+        private TcpClient client;
+        private StreamWriter sWriter;
+        private StreamReader sReader;
+
+        public OurClient()
+        {
+            client = new TcpClient("127.0.0.1", 5555);
+            sWriter = new StreamWriter(client.GetStream(), Encoding.UTF8);
+            sReader = new StreamReader(client.GetStream(), Encoding.UTF8);
+
+            HandleCommunication();
+        }
+
+        void HandleCommunication()
+        {
+            while (true)
+            {
+                Console.Write("> ");
+                string message = Console.ReadLine();
+                sWriter.WriteLine(message);
+                sWriter.Flush();
+
+                string answerServer = sReader.ReadLine();
+                Console.WriteLine($"Сервер ответил -> {answerServer}");
+            }
+        }
+
+
+    }
+}
+
+// OurServer с получение ответа
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace Server
+{
+    class OurServer
+    {
+        TcpListener server;
+
+        public OurServer()
+        {
+            server = new TcpListener(IPAddress.Parse("127.0.0.1"), 5555);
+            server.Start();
+
+            LoopClients();  
+        }
+
+        void LoopClients()
+        {
+            while (true)
+            {
+                TcpClient client = server.AcceptTcpClient();
+
+                Thread thread = new Thread(() => HandleClient(client));
+                thread.Start();
+            }
+        }
+
+        void HandleClient(TcpClient client)
+        {
+            StreamReader sReader = new StreamReader(client.GetStream(), Encoding.UTF8);
+            StreamWriter sWriter = new StreamWriter(client.GetStream(), Encoding.UTF8);
+
+            while (true)
+            {
+                string message = sReader.ReadLine();
+                Console.WriteLine($"Клиент написал - {message}");
+
+                Console.WriteLine("Дайте сообщение клиенту: ");
+                string answer = Console.ReadLine();
+                sWriter.WriteLine(answer);
+                sWriter.Flush();
+            }
+        }
+    }
+}
+
+// OurServer без получения ответа
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace Server
+{
+    class OurServer
+    {
+        TcpListener server;
+
+        public OurServer()
+        {
+            server = new TcpListener(IPAddress.Parse("127.0.0.1"), 5555);
+            server.Start();
+
+            LoopClients();  
+        }
+
+        void LoopClients()
+        {
+            while (true)
+            {
+                TcpClient client = server.AcceptTcpClient();
+
+                Thread thread = new Thread(() => HandleClient(client));
+                thread.Start();
+            }
+        }
+
+        void HandleClient(TcpClient client)
+        {
+            StreamReader sReader = new StreamReader(client.GetStream(), Encoding.UTF8);
+            
+
+            while (true)
+            {
+                string message = sReader.ReadLine();
+                Console.WriteLine($"Клиент написал - {message}");
+            }
+        }
+    }
+}
+
+// OurClient буз получения ответа
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class OurClient
+    {
+        private TcpClient client;
+        private StreamWriter sWriter;
+
+        public OurClient()
+        {
+            client = new TcpClient("127.0.0.1", 5555);
+            sWriter = new StreamWriter(client.GetStream(), Encoding.UTF8);
+
+            HandleCommunication();
+        }
+
+        void HandleCommunication()
+        {
+            while (true)
+            {
+                Console.Write("> ");
+                string message = Console.ReadLine();
+                sWriter.WriteLine(message);
+                sWriter.Flush();
+            }
+        }
+
+
+    }
+}
