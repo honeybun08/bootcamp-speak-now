1378c750ad520bcc5ac5eda923cf5a23da4c0475
Ulyana <ulyana_yezhgurova@mail.ru>
2023-07-09 10:48:23 +0300

    Commented all versions of server

--- ServerTCP/Program.cs
+++ ServerTCP/Program.cs
@@ -1,178 +1,178 @@
 ﻿// program.cs с получение ответа
-﻿namespace Client
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Это наш клиент");
-            OurClient ourClient = new OurClient();
-        }
-    }
-}
+// ﻿namespace Client
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.WriteLine("Это наш клиент");
+//             OurClient ourClient = new OurClient();
+//         }
+//     }
+// }
 
-// OurClient с получение ответа
-using System.Net.Sockets;
-using System.Text;
+// // OurClient с получение ответа
+// using System.Net.Sockets;
+// using System.Text;
 
-namespace Client
-{
-    class OurClient
-    {
-        private TcpClient client;
-        private StreamWriter sWriter;
-        private StreamReader sReader;
+// namespace Client
+// {
+//     class OurClient
+//     {
+//         private TcpClient client;
+//         private StreamWriter sWriter;
+//         private StreamReader sReader;
 
-        public OurClient()
-        {
-            client = new TcpClient("127.0.0.1", 5555);
-            sWriter = new StreamWriter(client.GetStream(), Encoding.UTF8);
-            sReader = new StreamReader(client.GetStream(), Encoding.UTF8);
+//         public OurClient()
+//         {
+//             client = new TcpClient("127.0.0.1", 5555);
+//             sWriter = new StreamWriter(client.GetStream(), Encoding.UTF8);
+//             sReader = new StreamReader(client.GetStream(), Encoding.UTF8);
 
-            HandleCommunication();
-        }
+//             HandleCommunication();
+//         }
 
-        void HandleCommunication()
-        {
-            while (true)
-            {
-                Console.Write("> ");
-                string message = Console.ReadLine();
-                sWriter.WriteLine(message);
-                sWriter.Flush();
+//         void HandleCommunication()
+//         {
+//             while (true)
+//             {
+//                 Console.Write("> ");
+//                 string message = Console.ReadLine();
+//                 sWriter.WriteLine(message);
+//                 sWriter.Flush();
 
-                string answerServer = sReader.ReadLine();
-                Console.WriteLine($"Сервер ответил -> {answerServer}");
-            }
-        }
+//                 string answerServer = sReader.ReadLine();
+//                 Console.WriteLine($"Сервер ответил -> {answerServer}");
+//             }
+//         }
 
 
-    }
-}
+//     }
+// }
 
-// OurServer с получение ответа
-using System.Net.Sockets;
-using System.Net;
-using System.Text;
+// // OurServer с получение ответа
+// using System.Net.Sockets;
+// using System.Net;
+// using System.Text;
 
-namespace Server
-{
-    class OurServer
-    {
-        TcpListener server;
+// namespace Server
+// {
+//     class OurServer
+//     {
+//         TcpListener server;
 
-        public OurServer()
-        {
-            server = new TcpListener(IPAddress.Parse("127.0.0.1"), 5555);
-            server.Start();
+//         public OurServer()
+//         {
+//             server = new TcpListener(IPAddress.Parse("127.0.0.1"), 5555);
+//             server.Start();
 
-            LoopClients();  
-        }
+//             LoopClients();  
+//         }
 
-        void LoopClients()
-        {
-            while (true)
-            {
-                TcpClient client = server.AcceptTcpClient();
+//         void LoopClients()
+//         {
+//             while (true)
+//             {
+//                 TcpClient client = server.AcceptTcpClient();
 
-                Thread thread = new Thread(() => HandleClient(client));
-                thread.Start();
-            }
-        }
+//                 Thread thread = new Thread(() => HandleClient(client));
+//                 thread.Start();
+//             }
+//         }
 
-        void HandleClient(TcpClient client)
-        {
-            StreamReader sReader = new StreamReader(client.GetStream(), Encoding.UTF8);
-            StreamWriter sWriter = new StreamWriter(client.GetStream(), Encoding.UTF8);
+//         void HandleClient(TcpClient client)
+//         {
+//             StreamReader sReader = new StreamReader(client.GetStream(), Encoding.UTF8);
+//             StreamWriter sWriter = new StreamWriter(client.GetStream(), Encoding.UTF8);
 
-            while (true)
-            {
-                string message = sReader.ReadLine();
-                Console.WriteLine($"Клиент написал - {message}");
+//             while (true)
+//             {
+//                 string message = sReader.ReadLine();
+//                 Console.WriteLine($"Клиент написал - {message}");
 
-                Console.WriteLine("Дайте сообщение клиенту: ");
-                string answer = Console.ReadLine();
-                sWriter.WriteLine(answer);
-                sWriter.Flush();
-            }
-        }
-    }
-}
+//                 Console.WriteLine("Дайте сообщение клиенту: ");
+//                 string answer = Console.ReadLine();
+//                 sWriter.WriteLine(answer);
+//                 sWriter.Flush();
+//             }
+//         }
+//     }
+// }
 
-// OurServer без получения ответа
-using System.Net.Sockets;
-using System.Net;
-using System.Text;
+// // OurServer без получения ответа
+// using System.Net.Sockets;
+// using System.Net;
+// using System.Text;
 
-namespace Server
-{
-    class OurServer
-    {
-        TcpListener server;
+// namespace Server
+// {
+//     class OurServer
+//     {
+//         TcpListener server;
 
-        public OurServer()
-        {
-            server = new TcpListener(IPAddress.Parse("127.0.0.1"), 5555);
-            server.Start();
+//         public OurServer()
+//         {
+//             server = new TcpListener(IPAddress.Parse("127.0.0.1"), 5555);
+//             server.Start();
 
-            LoopClients();  
-        }
+//             LoopClients();  
+//         }
 
-        void LoopClients()
-        {
-            while (true)
-            {
-                TcpClient client = server.AcceptTcpClient();
+//         void LoopClients()
+//         {
+//             while (true)
+//             {
+//                 TcpClient client = server.AcceptTcpClient();
 
-                Thread thread = new Thread(() => HandleClient(client));
-                thread.Start();
-            }
-        }
+//                 Thread thread = new Thread(() => HandleClient(client));
+//                 thread.Start();
+//             }
+//         }
 
-        void HandleClient(TcpClient client)
-        {
-            StreamReader sReader = new StreamReader(client.GetStream(), Encoding.UTF8);
+//         void HandleClient(TcpClient client)
+//         {
+//             StreamReader sReader = new StreamReader(client.GetStream(), Encoding.UTF8);
             
 
-            while (true)
-            {
-                string message = sReader.ReadLine();
-                Console.WriteLine($"Клиент написал - {message}");
-            }
-        }
-    }
-}
+//             while (true)
+//             {
+//                 string message = sReader.ReadLine();
+//                 Console.WriteLine($"Клиент написал - {message}");
+//             }
+//         }
+//     }
+// }
 
-// OurClient буз получения ответа
-using System.Net.Sockets;
-using System.Text;
+// // OurClient буз получения ответа
+// using System.Net.Sockets;
+// using System.Text;
 
-namespace Client
-{
-    class OurClient
-    {
-        private TcpClient client;
-        private StreamWriter sWriter;
+// namespace Client
+// {
+//     class OurClient
+//     {
+//         private TcpClient client;
+//         private StreamWriter sWriter;
 
-        public OurClient()
-        {
-            client = new TcpClient("127.0.0.1", 5555);
-            sWriter = new StreamWriter(client.GetStream(), Encoding.UTF8);
+//         public OurClient()
+//         {
+//             client = new TcpClient("127.0.0.1", 5555);
+//             sWriter = new StreamWriter(client.GetStream(), Encoding.UTF8);
 
-            HandleCommunication();
-        }
+//             HandleCommunication();
+//         }
 
-        void HandleCommunication()
-        {
-            while (true)
-            {
-                Console.Write("> ");
-                string message = Console.ReadLine();
-                sWriter.WriteLine(message);
-                sWriter.Flush();
-            }
-        }
+//         void HandleCommunication()
+//         {
+//             while (true)
+//             {
+//                 Console.Write("> ");
+//                 string message = Console.ReadLine();
+//                 sWriter.WriteLine(message);
+//                 sWriter.Flush();
+//             }
+//         }
 
 
-    }
-}
+//     }
+// }
